176385ff62608a9946d6cbeaab1055972a3d1ead
Dan Kim <itsdanya@gmail.com>
2022-03-24 00:55:06 +0300

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
@@ -13,6 +14,30 @@ module.exports = {
         'sans': ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
       },
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: {
+        0: '#272727',
+        1: '#696969',
+        2: '#A9A9A9',
+        3: '#D4D4D4',
+        4: '#DEDEDE',
+        5: '#EFEFEF',
+        6: '#F6F6F6',
+      },
+      blue: {
+        0: '#345BFF',
+        1: '#A4B4F5',
+        2: '#E5EAFF',
+        3: '#EFF2FF',
+      },
+      red: {
+        0: '#FF344C',
+      }
+    }
   },
   plugins: [],
 }
