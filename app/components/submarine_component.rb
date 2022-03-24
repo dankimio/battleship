a58d3250786094a366f00453a1c9959960a30fee
Dan Kim <itsdanya@gmail.com>
2022-03-24 23:16:52 +0300

    Create SubmarineComponent

---
+++ app/components/submarine_component.rb
@@ -0,0 +1,21 @@
+class SubmarineComponent < ShipComponent
+  SIZE = 3
+
+  def initialize(direction:, position:, hits: [false, false, false])
+    super
+
+    if vertical?
+      @class_list << 'col-span-1'
+      @class_list << "row-span-#{SIZE}"
+    end
+
+    if horizontal?
+      @class_list << 'row-span-1'
+      @class_list << "col-span-#{SIZE}"
+    end
+  end
+
+  def classes
+    @class_list.uniq.join(' ')
+  end
+end
