435ffcff61afecdd652b30e42b6547eb8f4330d9
Dan Kim <itsdanya@gmail.com>
2022-03-27 00:42:58 +0300

    Create BoardNameComponent

---
+++ app/components/board_name_component.rb
@@ -0,0 +1,18 @@
+class BoardNameComponent < ViewComponent::Base
+  NAME = {
+    main: 'Your fleet',
+    opponent: 'Opponent'
+  }
+  # player_type: :main, :opponent
+  def initialize(player_type)
+    @player_type = player_type
+  end
+
+  def classes
+    @player_type.to_s
+  end
+
+  def name
+    NAME[@player_type]
+  end
+end
---
+++ test/components/board_name_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class BoardNameComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(BoardNameComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
