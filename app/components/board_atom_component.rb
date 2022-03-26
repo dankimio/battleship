98682ebd7d7e11269e896ec360512cd498bae6ef
Dan Kim <itsdanya@gmail.com>
2022-03-26 21:54:18 +0300

    Add .hit state to opponent's board

--- app/components/board_atom_component.rb
+++ app/components/board_atom_component.rb
@@ -1,9 +1,13 @@
 class BoardAtomComponent < ViewComponent::Base
-  def initialize(miss: false)
+  def initialize(miss: false, hit: false)
     @miss = miss
+    @hit = hit
   end
 
-  def miss?
-    @miss
+  def classes
+    result = []
+    result << 'miss' if @miss
+    result << 'hit' if @hit
+    result.join(' ')
   end
 end
