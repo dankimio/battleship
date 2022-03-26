07a6b9641975b99d4b46006fefe8518bb02c5ea8
Dan Kim <itsdanya@gmail.com>
2022-03-26 22:22:27 +0300

    Create Board

---
+++ app/models/board.rb
@@ -0,0 +1,28 @@
+class Board
+  SIZE = 10
+  PLAYER_TYPES = %i[main opponent]
+
+  attr_reader :player_type, :rows
+
+  # player_type: :main, :opponent
+  # rows: 10x10 array with board values
+  # - nil: default
+  # - false: miss
+  # - true: hit
+  def initialize(player_type, rows)
+    raise ArgumentError unless player_type.in?(PLAYER_TYPES)
+    @player_type = player_type
+
+    raise ArgumentError unless rows.size == SIZE
+    raise ArgumentError unless rows.flatten.size == SIZE * SIZE
+    @rows = rows
+  end
+
+  def self.empty(player_type)
+    Board.new(
+      player_type,
+      # 10x10 array with nil values
+      Array.new(10, Array.new(10, nil))
+    )
+  end
+end
