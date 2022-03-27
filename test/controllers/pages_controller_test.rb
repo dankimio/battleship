455a7a1f17cb3fb69500999b672e77dfb9b93e71
Dan Kim <itsdanya@gmail.com>
2022-03-27 04:35:07 +0300

    Add tests for PagesController

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get pages_index_url
+    get root_url
     assert_response :success
   end
 end
