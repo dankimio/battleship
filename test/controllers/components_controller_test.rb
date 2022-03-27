210cff91ca99a8424bade522bc58f8694c42a84b
Dan Kim <itsdanya@gmail.com>
2022-03-27 04:34:45 +0300

    Add tests for ComponentsController

--- test/controllers/components_controller_test.rb
+++ test/controllers/components_controller_test.rb
@@ -2,7 +2,32 @@ require 'test_helper'
 
 class ComponentsControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get components_index_url
+    get components_url
+    assert_response :success
+  end
+
+  test 'should get board' do
+    get board_components_url
+    assert_response :success
+  end
+
+  test 'should get colors' do
+    get colors_components_url
+    assert_response :success
+  end
+
+  test 'should get fonts' do
+    get fonts_components_url
+    assert_response :success
+  end
+
+  test 'should get interface' do
+    get interface_components_url
+    assert_response :success
+  end
+
+  test 'should get ships' do
+    get ships_components_url
     assert_response :success
   end
 end
