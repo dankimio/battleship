d5bdcdf8a7bf74c1bdecaca3af66c206e973c0a2
Dan Kim <itsdanya@gmail.com>
2022-03-29 19:28:34 +0300

    Add basic render tests for GraveyardComponent and ShipyardComponent

--- test/components/shipyard_component_test.rb
+++ test/components/shipyard_component_test.rb
@@ -1,10 +1,11 @@
 require 'test_helper'
 
 class ShipyardComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(ShipyardComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  setup do
+    render_inline(ShipyardComponent.new)
+  end
+
+  test 'should render' do
+    assert_selector('p', text: 'Shipyard')
   end
 end
