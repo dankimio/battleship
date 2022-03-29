60bd4e8f0cf9912c7c827804c4b2eaa1d3ed0847
Dan Kim <itsdanya@gmail.com>
2022-03-29 18:45:18 +0300

    Test BoardComponent

--- test/components/board_component_test.rb
+++ test/components/board_component_test.rb
@@ -1,12 +1,25 @@
-# frozen_string_literal: true
-
 require 'test_helper'
 
 class BoardComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(BoardComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  setup do
+    render_inline(BoardComponent.new(Board.empty(:main)))
+  end
+
+  test 'should render second board for ships' do
+    assert_selector('.board-main', count: 2)
+  end
+
+  test 'should render atoms' do
+    assert_selector('.board-atom', count: 100)
+  end
+
+  test 'should render numbers' do
+    assert_selector('.board-atom', count: 100)
+    assert_selector('.board-numbers > *', count: 10)
+  end
+
+  test 'should render letters' do
+    assert_selector('.board-atom', count: 100)
+    assert_selector('.board-letters > *', count: 10 + 2)
   end
 end
