5830d9de66b094b0b53057045cb8d1ce270c37d2
Dan Kim <itsdanya@gmail.com>
2022-03-29 19:13:12 +0300

    Test BoardAtomComponent

--- test/components/board_atom_component_test.rb
+++ test/components/board_atom_component_test.rb
@@ -1,10 +1,31 @@
 require 'test_helper'
 
 class BoardAtomComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(BoardAtomComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  test 'should render' do
+    render_inline(BoardAtomComponent.new)
+
+    assert_selector('.board-atom')
+    assert_no_selector('.board-atom > *')
+  end
+
+  test 'should render default state' do
+    render_inline(BoardAtomComponent.new(nil))
+
+    assert page.has_no_css?('.miss')
+    assert page.has_no_css?('.hit')
+  end
+
+  test 'should render missed state' do
+    render_inline(BoardAtomComponent.new(false))
+
+    assert page.has_css?('.miss')
+    assert page.has_no_css?('.hit')
+  end
+
+  test 'should render hit state' do
+    render_inline(BoardAtomComponent.new(true))
+
+    assert page.has_no_css?('.miss')
+    assert page.has_css?('.hit')
   end
 end
