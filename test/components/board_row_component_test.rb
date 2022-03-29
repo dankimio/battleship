2d19b5e7832836900281d7e39f0ebac645a2c7c3
Dan Kim <itsdanya@gmail.com>
2022-03-29 19:25:22 +0300

    Test BoardRowComponent

--- test/components/board_row_component_test.rb
+++ test/components/board_row_component_test.rb
@@ -1,10 +1,11 @@
 require 'test_helper'
 
 class BoardRowComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(BoardRowComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  setup do
+    render_inline(BoardRowComponent.new(Array.new(10, nil)))
+  end
+
+  test 'should render atoms' do
+    assert_selector('.board-atom', count: 10)
   end
 end
